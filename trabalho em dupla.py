0da2268c010362c95d9673d6c08dbf717f8288c4
DayCris1 <dayene_crist@hotmail.com>
2024-10-07 18:53:03 -0300

    trabalho em dupla
    escolha da classe, se barbaro, se mago ou arqueiro.

---
+++ trabalho em dupla.py
@@ -0,0 +1,14 @@
+#insira um numero para escolher a classe:
+
+numero=int(input("digite um numero"))
+
+if numero==1:
+        print(f"o numero {numero} pertence a classe barbaro")
+elif numero==2:
+        print(f"o numero {numero} pertence a classe mago ")
+elif numero==3:
+        print(f"o numero {numero} pertence a classe arqueiro")
+        
+
+    
+        
